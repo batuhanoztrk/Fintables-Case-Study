08b88f9ca3845c0d576cda8129dff59409691fe8
Batuhan Öztürk <batuhanoztrk@hotmail.com>
2024-06-09 14:31:38 +0300

    build: add react-native-reanimated and moti dependencies

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
 import "react-native-gesture-handler";
+import "react-native-reanimated";
 
 const queryClient = new QueryClient();
 
