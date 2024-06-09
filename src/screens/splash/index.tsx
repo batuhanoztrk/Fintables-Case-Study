b81b73a2f394f3118b0ad1ee07ba2093bd49b774
Batuhan Öztürk <batuhanoztrk@hotmail.com>
2024-06-09 14:36:46 +0300

    feat(splash): add expo-splash-screen integration

--- src/screens/splash/index.tsx
+++ src/screens/splash/index.tsx
@@ -1,6 +1,7 @@
 import { Screens } from "@constants/Screens";
 import { SplashScreenNavigationProp } from "@navigation/types";
 import { useNavigation } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { View } from "react-native";
 
@@ -8,10 +9,16 @@ const Splash = () => {
   const navigation = useNavigation<SplashScreenNavigationProp>();
 
   useEffect(() => {
-    navigation.reset({
-      index: 0,
-      routes: [{ name: Screens.Payments }],
-    });
+    SplashScreen.hideAsync()
+      .then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: Screens.Payments }],
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
 
   return <View style={{ flex: 1 }}></View>;
