71b93f2725fecb996b57e262f9a4785586c1e06d
Batuhan Öztürk <batuhanoztrk@hotmail.com>
2024-06-09 13:50:58 +0300

    build: configure project structure and dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@components": "./src/components",
+            "@constants": "./src/constants",
+            "@hooks": "./src/hooks",
+            "@navigation": "./src/navigation",
+            "@models": "./src/models",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@utils": "./src/utils",
+            "@assets": "./assets",
+            "@store": "./src/store",
+            "@root": "./",
+            "@app": "./src",
+          },
+        },
+      ],
+    ],
   };
 };
