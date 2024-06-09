e622a4443f5e10054fb983d67fc58de1d036fd47
Batuhan Öztürk <batuhanoztrk@hotmail.com>
2024-06-09 14:28:23 +0300

    feat: integrate react-query and TailwindCSS plugin

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import RootStackNavigator from "@app/navigation";
 import NavigationService from "@navigation/NavigationService";
 import { NavigationContainer } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
 import "react-native-gesture-handler";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <>
-      <NavigationContainer ref={(e) => NavigationService.setNavigator(e)}>
-        <RootStackNavigator />
-      </NavigationContainer>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer ref={(e) => NavigationService.setNavigator(e)}>
+          <RootStackNavigator />
+        </NavigationContainer>
+      </QueryClientProvider>
       <StatusBar style="dark" />
     </>
   );
