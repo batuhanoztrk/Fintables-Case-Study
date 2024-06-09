4af738880a40cce859f1919286ceab1bc074f441
Batuhan Öztürk <batuhanoztrk@hotmail.com>
2024-06-09 20:23:44 +0300

    refactor(Subscription): update UI and add icons

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -1,4 +1,5 @@
 import Color from "@constants/Color";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
@@ -6,49 +7,35 @@ type SubscriptionProps = {};
 
 const Subscription = ({ code, name, description, color, price }: any) => {
   return (
-    <View className="w-48 flex-1 gap-y-4" key={code}>
+    <View key={code} className="flex-row items-center rounded bg-gray-200">
       <View
-        className={`flex-1 overflow-hidden rounded-md border border-transparent`}
-        style={{
-          backgroundColor: color || Color.blue.primary,
-        }}
+        className="h-16 w-16 items-center justify-center rounded"
+        style={{ backgroundColor: color || Color.blue.primary }}
       >
-        <View className="flex-1 items-center gap-y-2 p-2">
-          <Text className="text-center text-xl font-semibold text-white">
-            {name}
-          </Text>
-          <Text className="text-sm text-white">{description}</Text>
-        </View>
-        <View
-          style={{
-            marginTop: 80,
-            backgroundColor: Color.white.DEFAULT,
-            height: 100,
-            borderRadius: 35,
-            left: 50,
-            transform: [{ rotate: "55deg" }],
-          }}
-        >
+        <View className="rounded-full bg-white px-2 py-0.5">
           <Text
-            className="text-black-primary text-center font-semibold"
-            style={{
-              marginTop: 40,
-              marginRight: 50,
-              transform: [{ rotate: "-55deg" }],
-            }}
+            className="text-center"
+            style={{ color: color || Color.blue.primary }}
           >
-            {price}$
+            {code}
           </Text>
         </View>
       </View>
-      <View className="px-4">
-        <TouchableOpacity
-          className="items-center justify-center rounded-md p-2.5"
-          style={{
-            backgroundColor: color || Color.blue.primary,
-          }}
-        >
-          <Text className="text-center font-medium text-white">Satın Al</Text>
+      <View className="ml-2 flex-1 flex-row items-center justify-center">
+        <View className="mr-2 flex-1">
+          <Text className="text-black-primary text-base font-semibold">
+            {name}
+          </Text>
+          <Text className="text-black-secondary text-sm">
+            Next: 30 December
+          </Text>
+        </View>
+        <Text className="text-black-primary mr-2 flex-[0.5] text-base font-bold">
+          ${price}
+          <Text className="text-sm font-normal">/mo</Text>
+        </Text>
+        <TouchableOpacity className="p-2">
+          <MaterialCommunityIcons name={"dots-vertical"} size={24} />
         </TouchableOpacity>
       </View>
     </View>
--- src/screens/profile/subscriptions/index.tsx
+++ src/screens/profile/subscriptions/index.tsx
@@ -3,7 +3,7 @@ import { subscriptions } from "@constants/Subscriptions";
 import { FlashList } from "@shopify/flash-list";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Dimensions, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { ScrollView } from "react-native-virtualized-view";
 
 const user = {
@@ -16,7 +16,7 @@ const Subscriptions = () => {
 
   return (
     <ScrollView className="flex-1 bg-white-light">
-      <View className="gap-y-4 p-4">
+      <View className="gap-y-2 p-4">
         <View className="gap-y-1">
           <Text className="text-black-primary text-3xl">
             {t("subscriptions.welcome", {
@@ -30,54 +30,14 @@ const Subscriptions = () => {
 
         <View className="gap-y-4">
           <Text className="text-black-primary text-xl font-bold">
-            Aktif Abonelikler
+            Abonelikler
           </Text>
           <View>
             <FlashList
-              horizontal={true}
-              pagingEnabled={true}
-              data={subscriptions}
-              showsHorizontalScrollIndicator={false}
-              renderItem={({ item, index }) => (
-                <View
-                  style={{
-                    width: Dimensions.get("window").width - 32,
-                  }}
-                  className="flex-1 flex-row"
-                >
-                  <View className="flex-[0.5] items-center justify-center gap-y-1">
-                    <Text className="text-black-primary text-center font-semibold">
-                      Başlangıç Tarihi
-                    </Text>
-                    <Text className="text-black-secondary text-center">
-                      03.03.2021
-                    </Text>
-                  </View>
-                  <Subscription {...item} />
-                  <View className="flex-[0.5] items-center justify-center gap-y-1">
-                    <Text className="text-black-primary text-center font-semibold">
-                      Bitiş Tarihi
-                    </Text>
-                    <Text className="text-black-secondary text-center">
-                      03.03.2021
-                    </Text>
-                  </View>
-                </View>
-              )}
-            />
-          </View>
-        </View>
-
-        <View className="gap-y-4">
-          <Text className="text-black-primary text-xl font-bold">
-            Geçmiş Abonelikler
-          </Text>
-          <View>
-            <FlashList
-              horizontal={true}
-              data={subscriptions}
-              showsHorizontalScrollIndicator={false}
-              ItemSeparatorComponent={() => <View className="w-4" />}
+              estimatedItemSize={64}
+              data={[...subscriptions, ...subscriptions, ...subscriptions]}
+              showsVerticalScrollIndicator={false}
+              ItemSeparatorComponent={() => <View className="h-4" />}
               renderItem={({ item, index }) => <Subscription {...item} />}
             />
           </View>
